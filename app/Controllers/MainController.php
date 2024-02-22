a1861621006479874d85379f14a23d5833e5391b
Coding Innovator <160553490+CodingInnovator@users.noreply.github.com>
2024-02-22 05:38:11 +0330

    add blade files

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -3,22 +3,31 @@
 namespace App\Controllers;
 
 use App\View;
+use Jenssegers\Blade\Blade;
 
 class MainController
 {
     private $view;
+    private $blade;
 
     public function __construct()
     {
         $this->view = new View;
+        $this->blade = new Blade('resources/views', 'resources/cache');
     }
 
     public function index()
     {
-        $this->view->renderView("app", [
+        echo $this->blade->render('app', [
             'name' => 'alireza tahriri',
             'email' => 'alirezatahriri4@gmail.com',
             'password' => md5('p@$$w0rd' . $_ENV['APP_KEY'])
         ]);
+
+        // $this->view->renderView("app", [
+        //     'name' => 'alireza tahriri',
+        //     'email' => 'alirezatahriri4@gmail.com',
+        //     'password' => md5('p@$$w0rd' . $_ENV['APP_KEY'])
+        // ]);
     }
 }
---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Information</title>
+    <link href="{{ $_ENV['APP_URL'] . '/resources/css/output.css' }}" rel="stylesheet">
+</head>
+
+<body class="font-english bg-gray-900 flex items-center justify-center h-screen">
+    <div class="bg-gray-800 text-white p-6 rounded-xl w-fit space-y-8">
+        <div class="space-y-3">
+            <h1 class="font-bold text-3xl">{{ $_ENV['APP_NAME'] }} Framework</h1>
+            <h2>You can send parameters from the controller and use them in views.</h2>
+        </div>
+        <div class="space-y-1">
+            <div>+ Name: {{ htmlspecialchars($name) }}</div>
+            <div>+ Email: {{ htmlspecialchars($email) }}</div>
+            <div>+ Password: {{ htmlspecialchars($password) }}</div>
+        </div>
+        <div>Your application URL: <a class="text-blue-500 underline" href="{{ $_ENV['APP_URL'] }}">{{ $_ENV['APP_URL'] }}</a></div>
+    </div>
+</body>
+
+</html>
